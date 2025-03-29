1351f0a8c5da6a480c4a7a4a291c45ca354d610d
Auwa <aurapasta@gmail.com>
2025-03-29 22:50:30 +0800

    Refactor carparkTypeFilter to improve indoor parking logic and streamline return values

--- App_Development/spotease/hooks/carparkTypeFilter.js
+++ App_Development/spotease/hooks/carparkTypeFilter.js
@@ -1,43 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 const indoorCarpark = [
-    "MULTI-STOREY CAR PARK",
-    "SURFACE/MULTI- CAR PARK",
-    "COVERED CAR PARK",
-    "BASEMENT CAR PARKSTOREY",
-    "MECHANISED CAR PARK"
+  "MULTI-STOREY CAR PARK",
+  "SURFACE/MULTI- CAR PARK",
+  "COVERED CAR PARK",
+  "BASEMENT CAR PARKSTOREY",
+  "MECHANISED CAR PARK",
 ];
 
 const recommendIndoor = [
-    "Light Rain",
-    "Moderate Rain",
-    "Heavy Rain",
-    "Thundery Showers",
-    "Heavy Thundery Showers",
-    "Heavy Thundery Showers with Gusty Winds"
+  "Light Rain",
+  "Moderate Rain",
+  "Heavy Rain",
+  "Thundery Showers",
+  "Heavy Thundery Showers",
+  "Heavy Thundery Showers with Gusty Winds",
 ];
 
 const carparkTypeFilter = ({ weatherForecastInput, carparkInfo }) => {
-    // Check if indoor parking is required based on the weather
-    const IndoorRequired = recommendIndoor.includes(weatherForecastInput);
+  // Check if indoor parking is required based on the weather
+  const IndoorRequired = recommendIndoor.includes(weatherForecastInput);
+  if (!IndoorRequired) {
+    return carparkInfo;
+  }
 
-    if (!IndoorRequired) {
-        return { filteredParking: carparkInfo };
-    }
-
-    for (let i = 0; i < carparkInfo.length; i++ )
-    {
-        if(indoorCarpark.includes(carparkInfo.CARPARK_TYPE)) filteredParking.push(carparkInfo[i])
-    }
-    return {filteredParking}
-
+  for (let i = 0; i < carparkInfo.length; i++) {
+    if (indoorCarpark.includes(carparkInfo.CARPARK_TYPE))
+      filteredParking.push(carparkInfo[i]);
+  }
+  return filteredParking;
 };
 
 export default carparkTypeFilter;
-
-
-
-
 
 //SURFACE CAR PARK
 // MULTI-STOREY CAR PARK
@@ -46,7 +40,6 @@ export default carparkTypeFilter;
 // COVERED CAR PARK
 // MECHANISED AND SURFACE CAR PARK
 // MECHANISED CAR PARK
-
 
 // Possible values for forecast include:
 // Fair
